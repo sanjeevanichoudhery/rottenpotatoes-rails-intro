34d4399285bd9c9e914624ac9274ee00df41ab38
Sanjeevani Choudhery <schoudhery@tamu.edu>
2019-09-30 06:14:07 +0000

    part2 filter and sort together

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,17 +13,16 @@ class MoviesController < ApplicationController
   def index
     @all_ratings=Movie.select(:rating).map(&:rating).uniq #https://stackoverflow.com/questions/8369812/rails-how-can-i-get-unique-values-from-column
     @selected_ratings = checked_ratings
-    @selected_ratings.each do |rating| #remember which selected on reload
-      params[rating] = true
-    end
     
     if params[:sort]
       @movies = Movie.order(params[:sort])
+      @movies = Movie.where(:rating => @selected_ratings)
       #https://stackoverflow.com/questions/19968638/refactoring-ruby-on-rails-link-to-with-sorting
     else
-      #@movies=Movie.all
+      @movies=Movie.all
+      @movies = Movie.where(:rating => @selected_ratings)
     end
-    @movies = Movie.where(:rating => @selected_ratings)
+    #@movies = Movie.where(:rating => @selected_ratings)
   end
 
   def new
